e5aedea3d402d3598d0e8d977b72ce81fce44506
Jayking Liu <398031097@qq.com>
2023-06-07 23:01:08 +0800

    载入字体第一次提交20230607-2301

---
+++ TFTChineseFont.ino
@@ -0,0 +1,24 @@
+#include <TFT_eSPI.h>
+#include "heiti_35.h"
+TFT_eSPI tft;
+void setup()
+{
+
+    Serial.begin(115200);
+    tft.begin();
+    tft.fillScreen(ST7735_BLACK);
+    tft.setTextColor(ST7735_WHITE);
+    tft.setTextFont(2);
+    tft.setRotation(1);
+    ledcSetup(1, 1000, 12);
+    ledcAttachPin(2, 1);
+    ledcWrite(1, pow(2, 10)); // 设置屏幕亮度为25%
+    tft.loadFont(heiti_35);
+    tft.setCursor(0,5);
+    tft.println("你好啊");
+    tft.unloadFont();
+    
+}
+void loop()
+{
+}
